f51abf0b2bdeb98936a8e8119ea38f21fc7b93cb
tomas6446 <tomas.kozakas@gmail.com>
2023-11-18 21:40:18 +0200

    Add extension

--- gnome/install_extension.sh
+++ gnome/install_extension.sh
@@ -7,6 +7,7 @@ https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/
 https://extensions.gnome.org/extension/5165/upower-battery/
 https://extensions.gnome.org/extension/615/appindicator-support/
 https://extensions.gnome.org/extension/5446/quick-settings-tweaker/
+https://extensions.gnome.org/extension/2645/brightness-control-using-ddcutil/
 )
 
 for i in "${array[@]}"
