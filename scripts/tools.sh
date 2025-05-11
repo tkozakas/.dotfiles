6b284ee7f4efcb2a5dc721d73325bbe71591932c
tkozakas <tomas.kozakas@gmail.com>
2025-05-11 22:02:36 +0300

    Remove

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-# Oh-My-Zsh
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    source "$HOME/.zshrc"
-fi
-
 # Sdkman
 if ! command -v sdk &> /dev/null; then
     curl -s https://get.sdkman.io | bash
