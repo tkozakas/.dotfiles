3e7c4d1840bbe592c5c04d7fce53b93c61f8f435
tkozakas <tomas.kozakas@gmail.com>
2025-05-11 22:08:33 +0300

    Corrections

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -3,13 +3,16 @@
 set -euo pipefail
 
 # Sdkman
+echo "Installing Sdkman..."
 if ! command -v sdk &> /dev/null; then
     curl -s https://get.sdkman.io | bash
-    source "$HOME/.sdkman/bin/sdkman-init.sh"
+    if [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]]; then
+        source "$HOME/.sdkman/bin/sdkman-init.sh"
+    fi
 fi
 
 # Uv
+echo "Installing Uv..."
 if ! command -v uv &> /dev/null; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
-    source "$HOME/.uv/bin/uv"
 fi
