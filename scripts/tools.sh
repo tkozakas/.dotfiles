8900569ae06381442c4c4772ade10482a5bdb2a9
tkozakas <tomas.kozakas@gmail.com>
2025-05-11 21:10:04 +0300

    Remove useless tools

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -5,46 +5,12 @@ set -euo pipefail
 # Sdkman
 if ! command -v sdk &> /dev/null; then
     curl -s "https://get.sdkman.io" | bash
-    source "$HOME/.sdkman/bin/sdkman-init.sh"
+    if [[ -f "$HOME/.sdkman/bin/sdkman-init.sh" ]]; then
+        source "$HOME/.sdkman/bin/sdkman-init.sh"
+    fi
 fi
 
 # Uv
 if ! command -v uv &> /dev/null; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
-fi
-
-# Docker
-if ! command -v docker &> /dev/null; then
-    if [[ -f /etc/os-release ]]; then
-        . /etc/os-release
-        case "$ID" in
-            arch)
-                sudo pacman -Syu --noconfirm --needed docker
-                ;;
-            fedora)
-                sudo dnf install -y moby-engine
-                ;;
-            ubuntu|debian)
-                sudo apt update && sudo apt install -y docker.io
-                ;;
-        esac
-    fi
-fi
-
-# Docker Compose
-if ! (docker compose version &> /dev/null) && ! (command -v docker-compose &> /dev/null); then
-    if [[ -f /etc/os-release ]]; then
-        . /etc/os-release
-        case "$ID" in
-            arch)
-                sudo pacman -Syu --noconfirm --needed docker-compose
-                ;;
-            fedora)
-                sudo dnf install -y docker-compose
-                ;;
-            ubuntu|debian)
-                sudo apt update && (sudo apt install -y docker-compose-v2 || sudo apt install -y docker-compose)
-                ;;
-        esac
-    fi
 fi
