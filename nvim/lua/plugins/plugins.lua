b3033df52c3947080b046083ef62768b7746b40f
tkozakas <tomas.kozakas@gmail.com>
2025-05-25 17:34:24 +0300

    Update stuff

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -20,4 +20,14 @@ return {
       },
     },
   },
+
+  require("neo-tree").setup({
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
+    },
+  }),
 }
