612a758b8d478e15cd146dcdcefcc6b6e50b107a
tkozakas <tomas.kozakas@gmail.com>
2025-11-15 10:36:24 +0200

    Update

--- nvim/lua/config/autocmds.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = "term://*",
-  callback = function()
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    vim.cmd("startinsert")
-  end,
-})
